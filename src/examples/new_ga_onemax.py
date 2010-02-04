40f59421b1bd3c422418290e351fcd6486d8e98e
fafor1 <none@none>
2010-02-04 18:23:04 -0500

    Updated ga-one-max to use the new module structure.

--- src/examples/new_ga_onemax.py
+++ src/examples/new_ga_onemax.py
@@ -1,29 +1,24 @@
 
-import random
 import eap.base as base
-import eap.evolver as evolver
-import eap.operators as operators
+import eap.toolbox as toolbox
 
-lEvolver = evolver.Evolver()
-lEvolver.register('fitness', base.Fitness, weights=(1.0,))
-lEvolver.register('individual', base.Individual, size=100,
-                fitness=lEvolver.fitness, generator=base.booleanGenerator())
-lEvolver.register('population', base.Population, size=300,
-                generator=lEvolver.individual)
-                
+lTools = toolbox.Toolbox()
+lTools.register('fitness', base.Fitness, weights=(1.0,))
+lTools.register('individual', base.Individual, size=100,
+                fitness=lTools.fitness, generator=base.booleanGenerator())
+lTools.register('population', base.Population, size=300,
+                generator=lTools.individual)
+
 def evalOneMax(individual):
     if not individual.mFitness.isValid():
         individual.mFitness.append(individual.count(True))
 
-lEvolver.register('evaluate', evalOneMax)
-lEvolver.register('crossover', operators.twoPointsCx)
-lEvolver.register('mutate', operators.flipBitMut, flipIndxPb=0.05)
-lEvolver.register('select', operators.tournSel, tournSize=3)
+lTools.register('evaluate', evalOneMax)
+lTools.register('crossover', toolbox.twoPointsCx)
+lTools.register('mutate', toolbox.flipBitMut, flipIndxPb=0.05)
+lTools.register('select', toolbox.tournSel, tournSize=3)
 
-lPop = lEvolver.population()
-
-# Evaluate the population
-map(lEvolver.evaluate, lPop)
+lPop = lTools.population()
 
 CXPB, MUTPB, NGEN = (0.5, 0.2, 40)
 
@@ -31,18 +26,18 @@ CXPB, MUTPB, NGEN = (0.5, 0.2, 40)
 for g in range(NGEN):
     print 'Generation', g
 
-    lPop[:] = lEvolver.select(lPop, n=len(lPop))
+    lPop[:] = lTools.select(lPop, n=len(lPop))
 
     # Apply crossover and mutation
     for i in xrange(1, len(lPop), 2):
         if random.random() < CXPB:
-            lPop[i - 1], lPop[i] = lEvolver.crossover(lPop[i - 1], lPop[i])
+            lPop[i - 1], lPop[i] = lTools.crossover(lPop[i - 1], lPop[i])
     for i in xrange(len(lPop)):
         if random.random() < MUTPB:
-            lPop[i] = lEvolver.mutate(lPop[i])
+            lPop[i] = lTools.mutate(lPop[i])
 
     # Evaluate the population
-    map(lEvolver.evaluate, lPop)
+    map(lTools.evaluate, lPop)
 
     # Gather all the fitnesses in one list and print the stats
     lFitnesses = [lInd.mFitness[0] for lInd in lPop]
