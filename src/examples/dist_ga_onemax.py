931dd8a411538a6dfbdab860133811b7bce5bea4
Félix-Antoine Fortin <felix.antoine.fortin@gmail.com>
2010-02-04 18:23:36 -0500

    Updated dist-ga_onemax to use the new module structure.

--- src/examples/dist_ga_onemax.py
+++ src/examples/dist_ga_onemax.py
@@ -1,36 +1,34 @@
 
 from mpi4py import MPI
 import eap.base as base
-import eap.evolver as evolver
-import eap.operators as operators
+import eap.toolbox as toolbox
 
-def distributedGA(evolver, population, cxPb, mutPb, nGen):
+def distributedGA(toolbox, population, cxPb, mutPb, nGen):
     # Evaluate the population
-    map(evolver.evaluate, lPop)
+    map(toolbox.evaluate, population)
     
     # Begin the evolution
     for g in range(nGen):
         print 'Generation', g
     
-        lPop[:] = evolver.select(lPop, n=len(lPop))
+        lPop[:] = toolbox.select(lPop, n=len(population))
     
         # Apply crossover and mutation
         for i in xrange(1, len(lPop), 2):
             if random.random() < cxPb:
-                lPop[i - 1], lPop[i] = evolver.crossover(lPop[i - 1], lPop[i])
-        for i in xrange(len(lPop)):
+                population[i - 1], population[i] = toolbox.crossover(population[i - 1], population[i])
+        for i, ind in enumerate(population):
             if random.random() < mutPb:
-                lPop[i] = evolver.mutate(lPop[i])
-        
-        lChilds = [dtm.spawn(evolver.evaluate, lInd) for lInd in lPop]
-            
+                lPop[i] = toolbox.mutate(ind)
+
+        # Distribute the evaluation
+        lChilds = [dtm.spawn(toolbox.evaluate, lInd) for lInd in population]
         lData = yield ('waitFor', lChilds)
-        
         for i, lID in enumerate(lChilds):
             lPop[i].mFitness.append(lData[lID])
     
         # Gather all the fitnesses in one list and print the stats
-        lFitnesses = [lInd.mFitness[0] for lInd in lPop]
+        lFitnesses = [lInd.mFitness[0] for lInd in population]
         print '\tMin Fitness :', min(lFitnesses)
         print '\tMax Fitness :', max(lFitnesses)
         print '\tMean Fitness :', sum(lFitnesses)/len(lFitnesses)
@@ -44,18 +42,17 @@ def evalOneMax(individual):
 
 
 if MPI.COMM_WORLD.Get_rank() == 0:
-    evolver.register('fitness', base.Fitness, weights=(1.0,))
-    evolver.register('individual', base.Individual, size=100,
-                    fitness=evolver.fitness, generator=base.booleanGenerator())
-    evolver.register('population', base.Population, size=300,
-                    generator=evolver.individual)
-    
-    evolver.register('evaluate', evalOneMax)
-    evolver.register('crossover', operators.twoPointsCx)
-    evolver.register('mutate', operators.flipBitMut, flipIndxPb=0.05)
-    evolver.register('select', operators.tournSel, tournSize=3)
-    evolver.register('evolve', evolver.distributedGA)
-    
-    lPop = evolver.population()
-    
-    evolver.evolve(lPop, cxPb=0.5, mutPb=0.2, nGen=40)
+    lTools = toolbox.Toolbox()
+    lTools.register('fitness', base.Fitness, weights=(1.0,))
+    lTools.register('individual', base.Individual, size=100,\
+                   fitness=lTools.fitness, generator=base.booleanGenerator())
+    lTools.register('population', base.Population, size=300,\
+                    generator=lTools.individual)
+
+    lTools.register('evaluate', evalOneMax)
+    lTools.register('crossover', toolbox.twoPointsCx)
+    lTools.register('mutate', toolbox.flipBitMut, flipIndxPb=0.05)
+    lTools.register('select', toolbox.tournSel, tournSize=3)
+
+    lPop = lTools.population()
+    dtm.spawn(distributedGA, lTools, lPop, 0.5, 0.2, 40)
