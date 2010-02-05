8d3a19c6a2b8112a3fc2cf17e9760373c72e6130
François-Michel De Rainville <f.derainville@gmail.com>
2010-02-05 13:54:26 -0500

    Forgot about import random ... now it is fixed

--- src/examples/ga_onemax.py
+++ src/examples/ga_onemax.py
@@ -13,6 +13,8 @@
 #    You should have received a copy of the GNU Lesser General Public
 #    License along with EAP. If not, see <http://www.gnu.org/licenses/>.
 
+import random
+
 import eap.base as base
 import eap.toolbox as toolbox
 
