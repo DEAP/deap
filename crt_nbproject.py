d714f9274918ce9203d48f2d070d655a6a49d668
nitrof <none@none>
2010-02-19 09:40:25 -0500

    Added a Python script to easily create the Netbeans project associated with this clone of EAP.

---
+++ crt_nbproject.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+""" 
+Simple script to automatically create the Netbeans Project folder
+and files. It selects the default python version as the interpreter.
+This can then be changed by modifying the project option in Netbeans.
+"""
+
+from __future__ import with_statement
+import sys
+import os
+from string import Template
+import commands
+
+prop_template = Template("""java.lib.path=
+platform.active=Python_$version
+python.lib.path=
+src.dir=$src
+""")
+
+xml_template = Template("""<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://www.netbeans.org/ns/project/1">
+    <type>org.netbeans.modules.python.project</type>
+    <configuration>
+        <data xmlns="http://nbpython.dev.java.net/ns/php-project/1">
+            <name>$name</name>
+            <sources>
+                <root id="src.dir"/>
+            </sources>
+            <tests/>
+        </data>
+    </configuration>
+</project>""")
+
+project_name = commands.getoutput('basename `pwd`')
+project_src = commands.getoutput('pwd')+ '/src'
+python_version = sys.version.partition(" ")[0]
+
+xml_string = xml_template.substitute(name=project_name)
+prop_string = prop_template.substitute(version=python_version, src=project_src)
+
+if not os.path.exists('nbproject'):
+    os.makedirs('nbproject')
+
+with open('nbproject/project.properties', 'w') as properties:
+    properties.write(prop_string)
+with open('nbproject/project.xml', 'w') as xml:
+    xml.write(xml_string)
+
