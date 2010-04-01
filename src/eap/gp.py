1ff897c6d650b6d74e0dfe9754a1b6dd0559a78b
Félix-Antoine Fortin <felix.antoine.fortin@gmail.com>
2010-04-01 11:14:32 -0400

    Introducing a new way of doing GP with EAP.
    gptoolbox module is now named gp.
    In gp:
    - removed ToolboxGP and introduced ProgramGenerator.
    - removed Function, UnaryOperation, BinaryOperation, replaced by
      Primitive.
    
    In base:
    - introduced a new N-Ary Tree class.
    
    In module toolbox:
    - removed one tree crossover and one tree mutation.
    - modified the remaining tree crossover and tree mutation to conform
      to the new interface.
    
    In examples:
    - Removed gp_symbreg_fast
    - Modified gp_symbreg entirely to match the new interface of gp, creator
      and toolbox.
    
    --HG--
    rename : src/eap/gptoolbox.py => src/eap/gp.py

---
+++ src/eap/gp.py
@@ -0,0 +1,73 @@
+import random
+from itertools import repeat
+
+def evaluate(expr):
+    try:
+        func = expr[0]
+        try:
+            return func(*[evaluate(value) for value in expr[1:]])
+        except TypeError:
+            return func(*expr[1:])
+    except TypeError:
+        return expr
+
+class Primitive(object):
+    def __init__(self, primitive, arity):
+        name = primitive.__name__
+        args = ", ".join(repeat("%s",arity))
+        self.seq = "%s(%s)" % (name, args)
+    def __call__(self, *args):
+        return self.seq % args
+
+class ProgramGenerator(object):
+    def __init__(self):
+        self.primitive_set = []
+        self.terminal_set = []
+        self.func_dict = dict()
+        
+    def addPrimitive(self, primitive, arity):
+        if arity > 0:
+            self.primitive_set.append((Primitive(primitive,arity), arity))
+            self.func_dict[primitive.__name__] = primitive
+
+    def addTerminal(self, terminal):    
+        if callable(terminal):
+            self.func_dict[terminal.__name__] = terminal
+        self.terminal_set.append(terminal)
+            
+    def generate(self, min, max):
+        termset_ratio = len(self.terminal_set) / \
+                        (len(self.terminal_set)+len(self.primitive_set))
+        def generate_expr(max_depth):
+            if max_depth == 0 or random.random() < termset_ratio:
+                term = random.choice(self.terminal_set)
+                if not callable(term):
+                    expr = [term]
+                else:
+                    expr = [term()]
+            else:
+                func, arity = random.choice(self.primitive_set)
+                expr = [func]
+                for i in xrange(arity):
+                    arg = generate_expr(max_depth-1)
+                    if len(arg) > 1:
+                        expr.append(arg)
+                    else:
+                        expr.extend(arg)
+            return expr
+        max_depth = random.randint(min, max)
+        return generate_expr(max_depth)
+
+    def lambdify(self, expr, args):
+        if isinstance(expr, list):
+            expr = evaluate(expr)
+
+        if isinstance(args, str):
+            pass
+        elif hasattr(args, "__iter__"):
+            args = ",".join(str(a) for a in args)
+        else:
+            args = str(args)
+        lstr = "lambda %s: (%s)" % (args, expr)
+        return eval(lstr, self.func_dict)
+
--- src/examples/gp_symbreg.py
+++ src/examples/gp_symbreg.py
@@ -13,71 +13,71 @@
 #    You should have received a copy of the GNU Lesser General Public
 #    License along with EAP. If not, see <http://www.gnu.org/licenses/>.
 
-import sympy
+import sys
+import os
 import random
+import operator
+import math
+import itertools
+
+sys.path.append(os.path.abspath('..'))
 
 import eap.base as base
+import eap.creator as creator
 import eap.toolbox as toolbox
+import eap.gp as gp
 import eap.algorithms as algorithms
 
-# define primitives
-def add(left, right):
-    return left + right
-def sub(left, right):
-    return left - right
-def mul(left, right):
-    return left * right
-def rdiv(left, right):
-    return sympy.nsimplify(left/right)
+random.seed(2)
 
-def randomCte():
-    return random.randint(-1,1)
+# Define new functions
+def safeDiv(left, right):
+    try:
+        return left / right
+    except ZeroDivisionError:
+        return 0
 
-# add primitives and closures to their respective list
-lFuncs = [add, sub, mul]
-# defines symbols that will be used in the expression
-lSymbols = [sympy.Symbol('x')]
-# define terminal set
-lTerms = [sympy.Rational(1)]
-# add the symbols to the terminal set as 0-arity functions.
-lTerms.extend([lambda: symb for symb in lSymbols])
+prog_gen = gp.ProgramGenerator()
+prog_gen.addPrimitive(operator.add, 2)
+prog_gen.addPrimitive(operator.sub, 2)
+prog_gen.addPrimitive(operator.mul, 2)
+prog_gen.addPrimitive(safeDiv, 2)
+prog_gen.addPrimitive(operator.neg, 1)
+prog_gen.addPrimitive(math.cos, 1)
+prog_gen.addPrimitive(math.sin, 1)
+prog_gen.addTerminal(lambda: random.randint(-1,1))
+prog_gen.addTerminal('x')
 
-lTools = toolbox.Toolbox()
-lTools.register('fitness', base.Fitness, weights=(-1.0,))
-lTools.register('expression', base.expressionGenerator, funcSet=lFuncs,
-		termSet=lTerms, maxDepth=3)
-lTools.register('individual', base.IndividualTree,
-                fitness=lTools.fitness, generator=lTools.expression())
-lTools.register('population', base.Population, size=100,
-                generator=lTools.individual)
+creator.create("Individual", (base.Tree,), {'fitness':base.Fitness})
+creator.create("Population", (base.List,))
 
-def evalSymbReg(individual, symbols):
-    if not individual.mFitness.isValid():
-        # Simplify the expression by collecting the terms
-        expr = individual.evaluate()
-        # Transform the symbolic expression in a callable function
-        lFuncExpr = sympy.lambdify(symbols, expr)
-        lDiff = 0
+tools = toolbox.Toolbox()
+tools.register('expr', prog_gen.generate, min=1, max=2)
+tools.register('individual', creator.Individual, content=tools.expr)
+tools.register('population', creator.Population, size=100, content=tools.individual)
+
+def evalSymbReg(individual):
+    if not individual.fitness.isValid():
+        # Transform the tree expression in a callable function
+        func = prog_gen.lambdify(individual, 'x')
         # Evaluate the sum of squared difference between the expression
-        # and the real function : x**4 + x**3 + x**2 + x + 1
-        for x in xrange(-10,10):
-            x = x/10.
-            try:
-                lDiff += (lFuncExpr(x)-(x**4 + x**3 + x**2 + x + 1))**2
-            except ZeroDivisionError:
-                lDiff += ((x**4 + x**3 + x**2 + x + 1))**2
+        # and the real function : x**4 + x**3 + x**2 + x
+        values = (x/10. for x in xrange(-10,10))
+        diff_func = lambda x: (func(x)-(x**4 + x**3 + x**2 + x))**2
+        diff = sum(map(diff_func, values))
+        individual.fitness.append(diff)
 
-        individual.mFitness.append(lDiff)
+tools.register('evaluate', evalSymbReg)
+tools.register('select', toolbox.tournSel, tournsize=3)
+tools.register('mate', toolbox.uniformOnePtTreeCx)
+tools.register('mutate', toolbox.uniformTreeMut, expression=tools.expr,
+		        min=0, max=2)
 
-lTools.register('evaluate', evalSymbReg, symbols=lSymbols)
-lTools.register('select', toolbox.tournSel, tournSize=3)
-lTools.register('mate', toolbox.uniformOnePtCxGP)
-lTools.register('mutate', toolbox.uniformTreeMut, treeGenerator=lTools.expression,
-		depthRange=(0,2))
+pop = tools.population()
+algorithms.simpleEA(tools, pop, 0.5, 0.2, 50)
+
+best = toolbox.bestSel(pop,1)[0]
+
+print 'Best individual : ', gp.evaluate(best), best.fitness
 
 
-lPop = lTools.population()
-algorithms.simpleEA(lTools, lPop, 0.5, 0.2, 40)
-
-lBest = toolbox.bestSel(lPop,1)[0]
-print 'Best individual : ', sympy.sympify(lBest.evaluate()), lBest.mFitness
