28717c7d18f8631f8410aca86e4fafc642766c7e
Félix-Antoine Fortin <felix.antoine.fortin@gmail.com>
2010-05-26 11:25:32 -0400

    Minor modifications in base.py : Tree: - Added some comments to the methods Fitness - Removed redundant comparison functions : gt, ge, ne and cmp - Reduced code in __deepcopy__ to one line that returns a Fitness with   the values correctly initialized. - Using imap in isDominated instead of multiplying values and weights in   the for loop. - Corrected comments.

--- eap/base.py
+++ eap/base.py
@@ -24,6 +24,7 @@ import array
 import copy
 import operator
 import random
+import copy
 
 from collections import deque
 from itertools import izip, repeat, count, chain, imap
@@ -34,7 +35,8 @@ class Tree(list):
     class Node(object):
         height = 0
         size = 1
-        @property
+
+        @property            
         def root(self):
             return self
         def __getstate__(self):
@@ -47,26 +49,31 @@ class Tree(list):
 
     @classmethod
     def create_node(cls, obj):
-        Node = type("Node", (type(obj),cls.Node), {})
-        Node.base = type(obj)
+        """ Creates a node that will be added to the Tree.
+        
+            A node is run-time defined class that inherits from the object
+            and the Node class. This inheritance add functionnality to the
+            that are defined in Node.
+        """
+        Node = type("Node", (type(obj),cls.Node), {"base": type(obj)})
         try:
             new_node = Node.__new__(Node)
             new_node.__dict__.update(obj.__dict__)
         except AttributeError:
             new_node = Node(obj)
-            
         return new_node
 
     @classmethod
     def rectify_subtree(cls, subtree):
+        """ Returns the root if a Tree is just composed of one node, otherwise
+            returns the tree.
+        """
         if subtree.size > 1:
             return subtree
         else:
             return subtree.root
 
     def __init__(self, content=None):
-        if hasattr(content, "__call__"):
-            content = content()
         for elem in content:
             if isinstance(elem, list):
                 self.append(Tree(elem))
@@ -74,17 +81,33 @@ class Tree(list):
                 self.append(Tree.create_node(elem))
     
     def __getstate__(self):
-        """ This methods returns the state of the Tree
+        """ Returns the state of the Tree
             as a list of arbitrary elements. It is mainly
             used for pickling a Tree object.
         """
-        return [elem.__getstate__() for elem in self]
-        
+        return [elem.__getstate__() for elem in self] 
+     
 #    def __setstate__(self, state):
 #        self.__init__(state)
     
     def __reduce__(self):
+        """ Returns the class init, the object's state and the object
+            dict in a tuple. 
+            
+            The function is used to pickle Tree.
+        """
         return (self.__class__, (self.__getstate__(),), self.__dict__)
+    
+    def __deepcopy__(self, memo):
+        """ Deepcopy a Tree by first converting it back to a list of list.
+        
+            This deepcopy is faster than the default implementation. From
+            quick testing, up to 1.6 times faster, and at least 2 times less
+            function calls.
+        """
+        new = self.__class__(self.__getstate__())
+        new.__dict__.update(copy.deepcopy(self.__dict__, memo))
+        return new
     
     @property
     def root(self):
@@ -93,16 +116,16 @@ class Tree(list):
 
     @property
     def size(self):
-        """ This method returns the number of nodes in the tree."""
+        """ Returns the number of nodes in the tree."""
         return sum(elem.size for elem in self)
 
     @property
     def height(self):
-        """ This method returns the height of the tree."""
+        """Returns the height of the tree."""
         return max(elem.height for elem in self)+1
 
     def search_subtree_dfs(self, index):
-        """ This method searches the subtree with the
+        """ Searches the subtree with the
             corresponding index based on a depth first
             search.
         """
@@ -118,7 +141,7 @@ class Tree(list):
             total += nbr_child
 
     def set_subtree_dfs(self, index, subtree):
-        """ This method replaced the tree with
+        """ Replaces the tree with
             the corresponding index by subtree based
             on a depth-first search.
         """
@@ -137,7 +160,7 @@ class Tree(list):
             total += nbr_child
 
     def search_subtree_bfs(self, index):
-        """ This method searches the subtree with the
+        """ Searches the subtree with the
             corresponding index based on a breadth-first
             search.
         """
@@ -153,7 +176,7 @@ class Tree(list):
         return subtree
 
     def set_subtree_bfs(self, index, subtree):
-        """ This method replaced the subtree with
+        """ Replaces the subtree with
             the corresponding index by subtree based
             on a breadth-first search.
         """
@@ -173,12 +196,10 @@ class Tree(list):
         parent[child] = Tree.rectify_subtree(subtree)
 
 class Fitness(object):
-    """The fitness is a measure of quality of a solution. The fitness
-    inheritates from the :class:`Array` class, so the number of objectives
-    depends on the lenght of the array.
+    """The fitness is a measure of quality of a solution.
 
     Fitnesses may be compared using the ``>``, ``<``, ``>=``, ``<=``, ``==``,
-    ``!=`` and :meth:`cmp` operators. The comparison of those operators is made
+    ``!=``. The comparison of those operators is made
     lexicographically. Maximization and minimization are taken
     care off by a multiplication between the :attr:`weights` and the fitness values.
     The comparison can be made between fitnesses of different size, if the
@@ -231,89 +252,55 @@ class Fitness(object):
                       "Set the fitness to a single value or a tupe of values.")
 
     def isDominated(self, other):
-        '''In addition to the comparaison operators that are used to sort
+        """In addition to the comparaison operators that are used to sort
         lexically the fitnesses, this method returns :data:`True` if this
         fitness is dominated by the *other* fitness and :data:`False` otherwise.
         The weights are used to compare minimizing and maximizing fitnesses. If
         there is more fitness values than weights, the las weight get repeated
         until the end of the comparaison.
-        '''
+        """
         not_equal = False
-        # Pad the weights with the last value
-#        weights = itertools.chain(self.weights, repeat(self.weights[-1]))
-        for weight, self_value, other_value in izip(self.weights, self, other):
-            self_w_value = self_value * weight
-            other_w_value = other_value * weight
-            if self_w_value > other_w_value:
+        self_values = imap(operator.mul, self.values, self.weights)
+        other_values = imap(operator.mul, other.values, other.weights)
+        for self_value, other_value in izip(self_values, other_values):
+            if self_value > other_value:
                 return False
-            elif self_w_value < other_w_value:
+            elif self_value < other_value:
                 not_equal = True
         return not_equal
-
-    def __gt__(self, other):
-        return not self.__le__(other)
-
-    def __ge__(self, other):
-        return not self.__lt__(other)
+        
+    def __le__(self, other):
+        if other in (None, tuple()):    # Protection against yamling
+            return False
+        # Apply the weights to the values
+        self_values = map(operator.mul, self.values, self.weights)
+        other_values = map(operator.mul, other.values, other.weights)
+        return self_values <= other_values
 
     def __lt__(self, other):
         if other in (None, tuple()):    # Protection against yamling
             return False
-        # Pad the weights with the last value
-        #weights = chain(self.weights, repeat(self.weights[-1]))
         # Apply the weights to the values
         self_values = map(operator.mul, self.values, self.weights)
         other_values = map(operator.mul, other.values, other.weights)
-        # Compare the results
-        #print "%r < %r = %s" % (self_values, other_values, self_values < other_values)
         return self_values < other_values
-
-    def __le__(self, other):
-        if other in (None, tuple()):    # Protection against yamling
-            return False
-        # Pad the weights with the last value
-        #weights = chain(self.weights, repeat(self.weights[-1]))
-        # Apply the weights to the values
-        self_values = map(operator.mul, self.values, self.weights)
-        other_values = map(operator.mul, other.values, other.weights)
-        # Compare the results
-        return self_values <= other_values
 
     def __eq__(self, other):
         if other in (None, tuple()):    # Protection against yamling
             return False
-        # Pad the weights with the last value
-        #weights = chain(self.weights, repeat(self.weights[-1]))
         # Apply the weights to the values
         self_values = map(operator.mul, self.values, self.weights)
         other_values = map(operator.mul, other.values, other.weights)
-        # Compare the results
         return self_values == other_values
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __cmp__(self, other):
-        if self > other:
-            return 1
-        elif other > self:
-            return -1
-        return 0
-    
     def __deepcopy__(self, memo):
-        cls = self.__class__
-        copy_ = cls.__new__(cls)
-        cls.__init__(copy_, None)
-        if self.values is not None:
-            # Warning : when copying the fitness, elements shall be immutable
-            copy_.values = tuple(self.values)
-        else:
-            copy_.values = None
-        memo[id(self)] = copy_
-        # Add a memo to tell not to copy the values again when updating the dict
-        memo[id(self.values)] = copy_.values
-        copy_.__dict__.update(copy.deepcopy(self.__dict__, memo))
-        return copy_
+        """Replaces the basic deepcopy function with a faster one.
+        
+           It assumes that the elements in the :attr:`values` tuple are 
+           immutable and the fitness does not contain any other object 
+           than :attr:`values` and :attr:`weights`.
+        """
+        return self.__class__(self.values)
     
     #def __repr__(self):
     #    return repr(self.values)
