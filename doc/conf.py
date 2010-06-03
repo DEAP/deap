1b06742ee1b5f8032c4a00068bdfde61a151f7e7
f.derainville <none@none>
2010-06-03 14:01:27 -0400

    Changed include directory

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ import sys
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append("../src")
+sys.path.append("..")
 #sys.path.append(os.path.abspath('_ext/'))
 
 import eap
