e7e6a5876bac93a8ee68bf89676e82d875abc73e
marc.andre.gardner <none@none>
2010-08-31 14:24:57 -0400

    Added the first version of DTM, and some test cases. Will be updated soon with more documentation.

---
+++ dtm/__init__.py
@@ -0,0 +1,6 @@
+# DTM : Distributed Task Manager
+# Pre-alpha version
+
+__author__ = "Marc-Andre Gardner"
+__version__ = "0.2"
+__revision__ = "0.2.1"
---
+++ eap/DemoDtm.py
@@ -0,0 +1,53 @@
+
+from taskmanager import DtmControl
+
+import time
+import math
+
+dtm = DtmControl()
+
+def foo(arg):
+    time.sleep(0.51)
+    return arg*(math.sqrt(math.pow(arg,2)))
+
+def bar(a, b, c):
+    time.sleep(0.5)
+    return a+b+c
+
+
+def primaryTest(nbr):
+    if nbr <= 2:
+        return True
+        
+    for i in xrange(2, int(math.sqrt(nbr))+1):
+        if nbr % i == 0:
+            return False
+    return True
+
+def main():
+    listInit = range(150)
+    print("Voici une liste :")
+    print(listInit)
+    print("\n")
+
+    asyncResult1 = dtm.apply_async(bar, [1,2,3], [4,5,6,7], c = [8,9])
+    weirdPow = dtm.map_async(foo, listInit)
+    
+    primes = dtm.filter(primaryTest, listInit)
+    print("Voici les nombres premiers :")
+    print(primes)
+    print("\n")
+
+    print("Est-ce que tous les resultats asynchrones sont prets? ", dtm.testAllAsync())
+    print("Attente de tous les resultats asynchrones")
+    dtm.waitForAll()
+    
+    print("Resultat de bar() calcule en meme temps :")
+    print(asyncResult1.get())
+
+    print("Resultat de foo() calcule en meme temps :")
+    print(weirdPow.get())
+
+dtm.start(main)
+
+    
