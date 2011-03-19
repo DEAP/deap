f1b0f630d548016f667e2014cd4463c9915f45ca
f.derainville <none@none>
2011-03-19 13:24:06 -0400

    Moved the History class to the operators module.
    --HG--
    branch : 0.7-dev

--- eap/operators.py
+++ eap/operators.py
@@ -13,11 +13,92 @@
 #    You should have received a copy of the GNU Lesser General Public
 #    License along with EAP. If not, see <http://www.gnu.org/licenses/>.
 
+import copy
 import math
 import random
 # Needed by Nondominated sorting
 from itertools import chain, cycle
 from operator import attrgetter
+
+class History(object):
+    """The :class:`History` class helps to build a genealogy of all the
+    individuals produced in the evolution. It contains two attributes,
+    the :attr:`genealogy_tree` that is a dictionary of lists indexed by
+    individual, the list contain the indices of the parents. The second
+    attribute :attr:`genealogy_history` contains every individual indexed
+    by their individual number as in the genealogy tree.
+    
+    The produced genealogy tree is compatible with `NetworkX
+    <http://networkx.lanl.gov/index.html>`_, here is how to plot the genealogy
+    tree ::
+    
+        hist = History()
+        
+        # Do some evolution and fill the history
+        
+        import matplotlib.pyplot as plt
+        import networkx as nx
+        
+        g = nx.DiGraph(hist.genealogy_tree)
+        nx.draw_springs(g)
+        plt.show()
+    
+    .. note::
+       The genealogy tree might get very big if your population and/or the 
+       number of generation is large.
+        
+    """
+    def __init__(self):
+        self.genealogy_index = 0
+        self.genealogy_history = dict()
+        self.genealogy_tree = dict()
+    
+    def populate(self, individuals):
+        """Populate the history with the initial *individuals*. An attribute
+        :attr:`history_index` is added to every individual, this index will 
+        help to track the parents and the children through evolution. This index
+        will be modified by the :meth:`update` method when a child is produced.
+        Modifying the internal :attr:`genealogy_index` of the history or the
+        :attr:`history_index` of an individual may lead to unpredictable
+        results and corruption of the history.
+        """
+        for ind in individuals:
+            self.genealogy_index += 1
+            ind.history_index = self.genealogy_index
+            self.genealogy_history[self.genealogy_index] = copy.deepcopy(ind)
+            self.genealogy_tree[self.genealogy_index] = list()
+        
+    def update(self, *individuals):
+        """Update the history with the new *individuals*. The index present
+        in their :attr:`history_index` attribute will be used to locate their
+        parents and modified to a unique one to keep track of those new
+        individuals.
+        """
+        parent_indices = [ind.history_index for ind in individuals]
+        
+        for ind in individuals:
+            self.genealogy_index += 1
+            ind.history_index = self.genealogy_index
+            self.genealogy_history[self.genealogy_index] = copy.deepcopy(ind)
+            self.genealogy_tree[self.genealogy_index] = parent_indices
+    
+    @property
+    def decorator(self):
+        """Property that returns an appropriate decorator to enhance the
+        operators of the toolbox. The returned decorator assumes that the
+        individuals are returned by the operator. First the decorator calls
+        the underlying operation and then calls the update function with what
+        has been returned by the operator as argument. Finally, it returns 
+        the individuals with their history parameters modified according to
+        the update function.
+        """
+        def decFunc(func):
+            def wrapFunc(*args, **kargs):
+                individuals = func(*args, **kargs)
+                self.update(*individuals)
+                return individuals
+            return wrapFunc
+        return decFunc
 
 ######################################
 # GA Crossovers                      #
