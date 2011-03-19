a90d61b59669023fefc7c7fc05a3b6c3fc44a381
f.derainville <none@none>
2011-03-19 13:36:31 -0400

    Moved HallOfFame and ParetoFront to operators module.
    --HG--
    branch : 0.7-dev

--- eap/operators.py
+++ eap/operators.py
@@ -13,12 +13,14 @@
 #    You should have received a copy of the GNU Lesser General Public
 #    License along with EAP. If not, see <http://www.gnu.org/licenses/>.
 
+import bisect
 import copy
 import math
 import random
 # Needed by Nondominated sorting
 from itertools import chain, cycle
 from operator import attrgetter
+import operator     # used in pareto-front hall of fame for eq
 from __future__ import division
 from collections import defaultdict
 
@@ -254,6 +256,130 @@ class Stats(object):
         values = zip(*(self.key(elem) for elem in seq))
         for key, func in self.functions.items():
             self.data[key].append(map(func, values))
+
+
+class HallOfFame(object):
+    """The hall of fame contains the best individual that ever lived in the
+    population during the evolution. It is sorted at all time so that the
+    first element of the hall of fame is the individual that has the best
+    first fitness value ever seen, according to the weights provided to the
+    fitness at creation time.
+    
+    The class :class:`HallOfFame` provides an interface similar to a list
+    (without being one completely). It is possible to retrieve its length,
+    to iterate on it forward and backward and to get an item or a slice from it.
+    """
+    def __init__(self, maxsize):
+        self.maxsize = maxsize
+        self.keys = list()
+        self.items = list()
+    
+    def update(self, population):
+        """Update the hall of fame with the *population* by replacing the worst
+        individuals in the it by the best individuals present in *population*
+        (if they are better). The size of the hall of fame is kept constant.
+        """
+        if len(self) < self.maxsize:
+            # Items are sorted with the best fitness first
+            self.items = sorted(chain(self, population), 
+                                key=attrgetter("fitness"), 
+                                reverse=True)[:self.maxsize]
+            self.items = [copy.deepcopy(item) for item in self.items]
+            # The keys are the fitnesses in reverse order to allow the use
+            # of the bisection algorithm 
+            self.keys = map(attrgetter("fitness"),
+                            reversed(self.items))
+        else:
+            for ind in population: 
+                if ind.fitness > self[-1].fitness:
+                    # Delete the worst individual from the front
+                    self.remove(-1)
+                    # Insert the new individual
+                    self.insert(ind)
+    
+    def insert(self, item):
+        """Insert a new individual in the hall of fame using the
+        :func:`~bisect.bisect_right` function. The inserted individual is
+        inserted on the right side of an equal individual. Inserting a new 
+        individual in the hall of fame also preserve the hall of fame's order.
+        This method **does not** check for the size of the hall of fame, in a
+        way that inserting a new individual in a full hall of fame will not
+        remove the worst individual to maintain a constant size.
+        """
+        item = copy.deepcopy(item)
+        i = bisect.bisect_right(self.keys, item.fitness)
+        self.items.insert(len(self) - i, item)
+        self.keys.insert(i, item.fitness)
+    
+    def remove(self, index):
+        """Remove the specified *index* from the hall of fame."""
+        del self.keys[len(self) - (index % len(self) + 1)]
+        del self.items[index]
+    
+    def clear(self):
+        """Clear the hall of fame."""
+        del self.items[:]
+        del self.keys[:]
+
+    def __len__(self):
+        return len(self.items)
+
+    def __getitem__(self, i):
+        return self.items[i]
+
+    def __iter__(self):
+        return iter(self.items)
+
+    def __reversed__(self):
+        return reversed(self.items)
+    
+    def __str__(self):
+        return str(self.items) + "\n" + str(self.keys)
+
+        
+class ParetoFront(HallOfFame):
+    """The Pareto front hall of fame contains all the non-dominated individuals
+    that ever lived in the population. That means that the Pareto front hall of
+    fame can contain an infinity of different individuals.
+    
+    The size of the front may become very large if it is used for example on
+    a continuous function with a continuous domain. In order to limit the number
+    of individuals, it is possible to specify a similarity function that will
+    return :data:`True` if the genotype of two individuals are similar. In that
+    case only one of the two individuals will be added to the hall of fame. By
+    default the similarity function is :func:`operator.__eq__`.
+    
+    Since, the Pareto front hall of fame inherits from the :class:`HallOfFame`, 
+    it is sorted lexicographically at every moment.
+    """
+    def __init__(self, similar=operator.eq):
+        self.similar = similar
+        HallOfFame.__init__(self, None)
+    
+    def update(self, population):
+        """Update the Pareto front hall of fame with the *population* by adding 
+        the individuals from the population that are not dominated by the hall
+        of fame. If any individual in the hall of fame is dominated it is
+        removed.
+        """
+        for ind in population:
+            is_dominated = False
+            has_twin = False
+            to_remove = []
+            for i, hofer in enumerate(self):    # hofer = hall of famer
+                if ind.fitness.isDominated(hofer.fitness):
+                    is_dominated = True
+                    break
+                elif hofer.fitness.isDominated(ind.fitness):
+                    to_remove.append(i)
+                elif ind.fitness == hofer.fitness and self.similar(ind, hofer):
+                    has_twin = True
+                    break
+            
+            for i in reversed(to_remove):       # Remove the dominated hofer
+                self.remove(i)
+            if not is_dominated and not has_twin:
+                self.insert(ind)
 
 ######################################
 # GA Crossovers                      #
