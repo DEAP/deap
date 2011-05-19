3142f33ede1869fcadfe68e57b0bfd4ebbb92997
felix.antoine.fortin <none@none>
2011-05-19 12:06:27 -0400

    Refactore how solutions are generated in generate and update functions to speed things up a bit.
    Remove the test functions from the cma module in order to eventually
    put them in a dedicated module, giving the opportunity to all examples
    to have access to benchmark functions.
    
    --HG--
    branch : 0.7-dev

--- eap/cma.py
+++ eap/cma.py
@@ -22,7 +22,7 @@ Evolution Strategy.
 """
 
 import logging
-from math import sqrt, cos, log, pi
+from math import sqrt, log
 import numpy
 import random   # Only used to seed numpy.random
 import sys      # Used to get maxint
@@ -131,8 +131,9 @@ class CMAStrategy(object):
         """Generate a population from the current strategy using the 
         centroid individual as parent.
         """
-        arz = numpy.random.randn(self.lambda_, self.dim)
-        return [ind_init(self.centroid + self.sigma * numpy.dot(self.BD, arzi)) for arzi in arz]
+        arz = numpy.random.standard_normal((self.lambda_, self.dim))
+        arz = self.centroid + self.sigma * numpy.dot(arz, self.BD.T)
+        return [ind_init(arzi) for arzi in arz]        
         
     def update(self, population):
         """Update the current covariance matrix strategy.
@@ -156,18 +157,18 @@ class CMAStrategy(object):
                
         self.update_count += 1
                
-        self.pc = (1 - self.cc) * self.pc \
-             + hsig * (sqrt(self.cc * (2 - self.cc) * self.mueff) / \
-                     self.sigma) * c_diff
-        
+        self.pc = (1 - self.cc) * self.pc + hsig \
+                  * sqrt(self.cc * (2 - self.cc) * self.mueff) / self.sigma \
+                  * c_diff
+                   
         # Update covariance matrix
         artmp = population[0:self.mu] - old_centroid
         self.C = (1 - self.ccov1 - self.ccovmu + (1 - hsig) \
                    * self.ccov1 * self.cc * (2 - self.cc)) * self.C \
-                 + numpy.outer(self.ccov1 * self.pc, self.pc) \
-                 + self.ccovmu \
-                   * numpy.dot(artmp.T , (self.weights * artmp.T).T) \
-                   / self.sigma**2
+                + self.ccov1 * numpy.outer(self.pc, self.pc) \
+                + self.ccovmu * numpy.dot((self.weights * artmp.T), artmp) \
+                / self.sigma**2
+                
         
         self.sigma *= numpy.exp((numpy.linalg.norm(self.ps) / self.chiN - 1.) \
                                 * self.cs / self.damps)
@@ -180,11 +181,12 @@ class CMAStrategy(object):
         self.diagD = self.diagD[indx]**0.5
         self.B = self.B[:,indx]
         self.BD = self.B * self.diagD
-        
-        arz = numpy.random.randn(self.lambda_, self.dim)
+            
+        arz = numpy.random.standard_normal((self.lambda_, self.dim))
+        arz = self.centroid + self.sigma * numpy.dot(arz, self.BD.T)
         for ind, arzi in zip(population, arz):
             del ind[:]
-            ind.extend(self.centroid + self.sigma * numpy.dot(self.BD, arzi))
+            ind.extend(arzi)
 
     def computeParams(self, params):
         """Those parameters depends on lambda and need to computed again if it 
@@ -217,33 +219,3 @@ class CMAStrategy(object):
         self.damps = 1. + 2. * max(0, sqrt((self.mueff - 1.) / \
                                             (self.dim + 1.)) - 1.) + self.cs
         self.damps = params.get("damps", self.damps)
-        
-
-def rand(individual):
-    """Random test objective function."""
-    return numpy.random.random()
-    
-def plane(individual):
-    """Plane test objective function."""
-    return individual[0]
-
-def rastrigin(individual):
-    """Rastrigin test objective function. Consider using ``lambda_ = 20 * N`` 
-    for this test function.
-    """
-    return 10 * len(individual) + sum(gene * gene - 10 * \
-                        cos(2 * pi * gene) for gene in individual)
-    
-def sphere(individual):
-    """Sphere test objective function."""
-    return sum(gene * gene for gene in individual)
-
-def cigar(individual):
-    """Cigar test objective function."""
-    return individual[0]**2 + 1e6 * sum(gene * gene for gene in individual)
-
-def rosenbrock(individual):  
-    """Rosenbrock test objective function."""
-    return sum(100 * (x * x - y)**2 + (1. - x)**2 \
-                   for x, y in zip(individual[:-1], individual[1:]))
-    
