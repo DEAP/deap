08504bf710f188f1376a65c68dbe1720268e272a
felix.antoine.fortin <none@none>
2011-05-19 10:39:46 -0400

    Added EAP header to de_sphere.py.
    --HG--
    branch : 0.7-dev

--- examples/de_sphere.py
+++ examples/de_sphere.py
@@ -1,3 +1,18 @@
+#    This file is part of EAP.
+#
+#    EAP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 3 of
+#    the License, or (at your option) any later version.
+#
+#    EAP is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with EAP. If not, see <http://www.gnu.org/licenses/>.
+
 from eap import base
 from eap import operators
 from eap import creator
