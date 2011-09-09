ebd32d4189768efc3afe5576405eb13f0efa70ab
Félix-Antoine Fortin <felix.antoine.fortin@gmail.com>
2011-09-09 11:45:25 -0400

    Added a new example modeled on the original NSGA-II algorithms by K. Deb which provides similar performance to the one presented in the original article.
    --HG--
    branch : 0.8-dev

---
+++ examples/ga_nsga2.py
@@ -0,0 +1,122 @@
+#    This file is part of DEAP.
+#
+#    DEAP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 3 of
+#    the License, or (at your option) any later version.
+#
+#    DEAP is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with DEAP. If not, see <http://www.gnu.org/licenses/>.
+
+import array
+import random
+import json
+
+from math import sqrt
+
+from deap import algorithms
+from deap import base
+from deap import benchmarks
+from deap.benchmarks.tools import diversity, convergence
+from deap import creator
+from deap import tools
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0, -1.0))
+creator.create("Individual", array.array, typecode='d', fitness=creator.FitnessMin)
+
+toolbox = base.Toolbox()
+
+# Problem definition
+BOUND_LOW, BOUND_UP = 0.0, 1.0
+NDIM = 30
+toolbox.register("evaluate", benchmarks.zdt1)
+
+# Attribute generator
+toolbox.register("attr_float", random.uniform, BOUND_LOW, BOUND_UP)
+
+# Structure initializers
+toolbox.register("individual", tools.initRepeat, creator.Individual,
+                 toolbox.attr_float, NDIM)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+toolbox.register("mate", tools.cxSimulatedBinaryBounded, low=BOUND_LOW, up=BOUND_UP, eta=20.0)
+toolbox.register("mutate", tools.mutPolynomialBounded, low=BOUND_LOW, up=BOUND_UP, eta=20.0, indpb=1.0/NDIM)
+toolbox.register("select", tools.selNSGA2)
+
+def main(seed=None):
+    random.seed(seed)
+
+    NGEN = 250
+    MU = 100
+    VARPB = 0.9
+
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("Avg", tools.mean)
+    stats.register("Std", tools.std)
+    stats.register("Min", min)
+    stats.register("Max", max)
+    
+    logger = tools.EvolutionLogger(stats)
+    logger.start()
+    
+    pop = toolbox.population(n=MU)
+
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in pop if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    # This is just to assign the crowding distance to the individuals
+    # no actual selection is done
+    pop = toolbox.select(pop, len(pop))
+    
+    stats.update(pop)
+    logger.log_gen(len(invalid_ind), 0)
+
+    # Begin the generational process
+    for gen in range(1, NGEN+1):
+        # Variate the population
+        offspring = tools.selTournamentDCD(pop, len(pop))
+        offspring = [toolbox.clone(ind) for ind in offspring]
+        
+        for ind1, ind2 in zip(offspring[::2], offspring[1::2]):
+            if random.random() <= VARPB:
+                toolbox.mate(ind1, ind2)
+            
+            toolbox.mutate(ind1)
+            toolbox.mutate(ind2)
+            del ind1.fitness.values, ind2.fitness.values
+        
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        # Select the next generation population
+        pop = toolbox.select(pop + offspring, MU)
+        stats.update(pop)
+        logger.log_gen(len(invalid_ind), gen)
+
+    return pop
+        
+if __name__ == "__main__":
+    optimal_front = json.load(open("zdt1_front.json"))
+    optimal_front = [optimal_front[i] for i in range(0, len(optimal_front), 2)]
+
+    first_point_opt = optimal_front[0]
+    last_point_opt = optimal_front[-1]
+
+    pop = main()
+    first_front = tools.sortFastND(pop, len(pop), True)[0]
+    first_front.sort(key=lambda x: x.fitness.values)
+    print "Diversity : ", diversity(first_front, first_point_opt, last_point_opt)
+    print "Convergence : ", convergence(first_front, optimal_front)
+
+
