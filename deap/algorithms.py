96e38dcba017c23542faf8b57ea3006994985cd0
François-Michel De Rainville <f.derainville@gmail.com>
2011-10-06 15:17:27 -0400

    Added precisions on varAnd.
    --HG--
    branch : 0.8-dev

--- deap/algorithms.py
+++ deap/algorithms.py
@@ -72,7 +72,10 @@ def varAnd(toolbox, population, cxpb, mutpb):
     :math:`P_\mathrm{o}` is returned.
     
     This variation is named *And* beceause of its propention to apply both
-    crossover and mutation on the individuals. Both probabilities should be in
+    crossover and mutation on the individuals. Note that both operators are
+    not applied systematicaly, the resulting individuals can be generated from
+    crossover only, mutation only, crossover and mutation, and reproduction
+    according to the given probabilities. Both probabilities should be in
     :math:`[0, 1]`.
     """
     offspring = [toolbox.clone(ind) for ind in population]
