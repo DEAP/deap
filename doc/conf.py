7a7a80b90df6b6c44d9ce043d63a2bd05c011ee2
François-Michel De Rainville <f.derainville@gmail.com>
2012-03-04 09:55:59 -0500

    Added a new theme (based on flask).
    --HG--
    branch : 0.8-dev

--- doc/conf.py
+++ doc/conf.py
@@ -126,17 +126,17 @@ plot_html_show_formats = True
 
 # -- Options for HTML output ---------------------------------------------------
 
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = ["_themes"]
+
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+html_theme = 'flask'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'collapsiblesidebar': True}
-
-# Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+#html_theme_options = {'collapsiblesidebar': True}
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -147,7 +147,7 @@ html_theme_options = {'collapsiblesidebar': True}
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+#html_logo = ""
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -157,7 +157,7 @@ html_theme_options = {'collapsiblesidebar': True}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
