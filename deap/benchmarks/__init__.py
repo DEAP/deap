3ae2e87979f39acaceb3588a48f7177e64f00c7b
François-Michel De Rainville <f.derainville@gmail.com>
2012-03-30 10:34:00 -0400

    Changed the dtlz1 documentation formulas for a product version and added clarifications on x_m.
    --HG--
    branch : 0.8-dev

--- deap/benchmarks/__init__.py
+++ deap/benchmarks/__init__.py
@@ -333,11 +333,19 @@ def dtlz1(individual, obj):
 
     :math:`g(\\mathbf{x}_m) = 100\\left(|\\mathbf{x}_m| + \sum_{x_i \in \\mathbf{x}_m}\\left((x_i - 0.5)^2 - \\cos(20\pi(x_i - 0.5))\\right)\\right)`
 
-    :math:`f_{\\text{DTLZ1}1}(\\mathbf{x}) = 0.5 x_1 \\dots x_{m-1}(1 + g(\\mathbf{x}_m))`
+    :math:`f_{\\text{DTLZ1}1}(\\mathbf{x}) = \\frac{1}{2} (1 + g(\\mathbf{x}_m)) \\prod_{i=1}^{m-1}x_i`
     
-    :math:`f_{\\text{DTLZ1}2}(\\mathbf{x}) = 0.5 x_1 \\dots x_{m-i}(1 + g(\\mathbf{x}_m))`
+    :math:`f_{\\text{DTLZ1}2}(\\mathbf{x}) = \\frac{1}{2} (1 + g(\\mathbf{x}_m)) (1-x_{m-1}) \\prod_{i=1}^{m-2}x_i`
     
-    :math:`f_{\\text{DTLZ1}m}(\\mathbf{x}) = 0.5 (1 - x_1)(1 + g(\\mathbf{x}_m))`
+    :math:`\\ldots`
+    
+    :math:`f_{\\text{DTLZ1}m-1}(\\mathbf{x}) = \\frac{1}{2} (1 + g(\\mathbf{x}_m)) (1 - x_2) x_1`
+    
+    :math:`f_{\\text{DTLZ1}m}(\\mathbf{x}) = \\frac{1}{2} (1 - x_1)(1 + g(\\mathbf{x}_m))`
+    
+    Where :math:`m` is the number of objectives and :math:`\\mathbf{x}_m` is a
+    vector of the remaining attributes :math:`[x_m~\\ldots~x_n]` of the
+    individual in :math:`n > m` dimensions.
     
     """
     g = 100 * (len(individual[obj-1:]) + sum((xi-0.5)**2 - cos(20*pi*(xi-0.5)) for xi in individual[obj-1:]))
@@ -350,15 +358,20 @@ def dtlz2(individual, obj):
     The individual must have at least *obj* element.
     From: K. Deb, L. Thiele, M. Laumanns and E. Zitzler. Scalable Multi-Objective 
     Optimization Test Problems. CEC 2002, p. 825 - 830, IEEE Press, 2002.
-
+    
     :math:`g(\\mathbf{x}_m) = \\sum_{x_i \in \\mathbf{x}_m} (x_i - 0.5)^2`
-
+    
     :math:`f_{\\text{DTLZ2}1}(\\mathbf{x}) = (1 + g(\\mathbf{x}_m)) \\prod_{i=1}^{m-1} \\cos(0.5x_i\pi)`
-
+    
     :math:`f_{\\text{DTLZ2}2}(\\mathbf{x}) = (1 + g(\\mathbf{x}_m)) \\sin(0.5x_{m-1}\pi ) \\prod_{i=1}^{m-2} \\cos(0.5x_i\pi)`
-
+    
+    :math:`\\ldots`
+    
     :math:`f_{\\text{DTLZ2}m}(\\mathbf{x}) = (1 + g(\\mathbf{x}_m)) \\sin(0.5x_{1}\pi )`
-
+    
+    Where :math:`m` is the number of objectives and :math:`\\mathbf{x}_m` is a
+    vector of the remaining attributes :math:`[x_m~\\ldots~x_n]` of the
+    individual in :math:`n > m` dimensions.
     """
     xc = individual[:obj-1]
     xm = individual[obj-1:]
@@ -372,15 +385,20 @@ def dtlz3(individual, obj):
     The individual must have at least *obj* element.
     From: K. Deb, L. Thiele, M. Laumanns and E. Zitzler. Scalable Multi-Objective 
     Optimization Test Problems. CEC 2002, p. 825 - 830, IEEE Press, 2002.
-
+    
     :math:`g(\\mathbf{x}_m) = 100\\left(|\\mathbf{x}_m| + \sum_{x_i \in \\mathbf{x}_m}\\left((x_i - 0.5)^2 - \\cos(20\pi(x_i - 0.5))\\right)\\right)`
-
+    
     :math:`f_{\\text{DTLZ3}1}(\\mathbf{x}) = (1 + g(\\mathbf{x}_m)) \\prod_{i=1}^{m-1} \\cos(0.5x_i\pi)`
-
+    
     :math:`f_{\\text{DTLZ3}2}(\\mathbf{x}) = (1 + g(\\mathbf{x}_m)) \\sin(0.5x_{m-1}\pi ) \\prod_{i=1}^{m-2} \\cos(0.5x_i\pi)`
-
+    
+    :math:`\\ldots`
+    
     :math:`f_{\\text{DTLZ3}m}(\\mathbf{x}) = (1 + g(\\mathbf{x}_m)) \\sin(0.5x_{1}\pi )`
-
+    
+    Where :math:`m` is the number of objectives and :math:`\\mathbf{x}_m` is a
+    vector of the remaining attributes :math:`[x_m~\\ldots~x_n]` of the
+    individual in :math:`n > m` dimensions.
     """
     xc = individual[:obj-1]
     xm = individual[obj-1:]
@@ -394,15 +412,20 @@ def dtlz4(individual, obj, alpha):
     The individual must have at least *obj* element.
     From: K. Deb, L. Thiele, M. Laumanns and E. Zitzler. Scalable Multi-Objective 
     Optimization Test Problems. CEC 2002, p. 825 - 830, IEEE Press, 2002.
-
+    
     :math:`g(\\mathbf{x}_m) = \\sum_{x_i \in \\mathbf{x}_m} (x_i - 0.5)^2`
-
+    
     :math:`f_{\\text{DTLZ4}1}(\\mathbf{x}) = (1 + g(\\mathbf{x}_m)) \\prod_{i=1}^{m-1} \\cos(0.5x_i^\\alpha\pi)`
-
+    
     :math:`f_{\\text{DTLZ4}2}(\\mathbf{x}) = (1 + g(\\mathbf{x}_m)) \\sin(0.5x_{m-1}^\\alpha\pi ) \\prod_{i=1}^{m-2} \\cos(0.5x_i^\\alpha\pi)`
-
+    
+    :math:`\\ldots`
+    
     :math:`f_{\\text{DTLZ4}m}(\\mathbf{x}) = (1 + g(\\mathbf{x}_m)) \\sin(0.5x_{1}^\\alpha\pi )`
-
+    
+    Where :math:`m` is the number of objectives and :math:`\\mathbf{x}_m` is a
+    vector of the remaining attributes :math:`[x_m~\\ldots~x_n]` of the
+    individual in :math:`n > m` dimensions.
     """
     xc = individual[:obj-1]
     xm = individual[obj-1:]
