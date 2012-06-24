a09171a17dcdfceafbfa6127c64bf644f7d74ad7
felix.antoine.fortin <none@none>
2012-06-24 00:38:40 -0400

    Corrected a bug in mutInsert and made random.choice local.
    The terminal was assumed to be callable, which is no longer
    the case. We now have to make sure the terminal is an
    EphemeralGenerator before calling it. Also made random.choice
    local to shorten lines.
    
    --HG--
    branch : dev

--- deap/gp.py
+++ deap/gp.py
@@ -623,6 +623,7 @@ def mutInsert(individual):
     index = random.randrange(len(individual))
     node = individual[index]
     slice_ = individual.searchSubtree(index)
+    choice = random.choice
     
     # As we want to keep the current node as children of the new one,
     # it must accept the return value of the current node
@@ -631,12 +632,16 @@ def mutInsert(individual):
     if len(primitives) == 0:
         return individual,
     
-    new_node = random.choice(primitives)
+    new_node = choice(primitives)
     new_subtree = [None] * len(new_node.args)
-    position = random.choice([i for i, a in enumerate(new_node.args) if a == node.ret])
+    position = choice([i for i, a in enumerate(new_node.args) if a == node.ret])
+    
     for i, arg_type in enumerate(new_node.args):
         if i != position:
-            new_subtree[i] = random.choice(pset.terminals[arg_type])()
+            term = choice(pset.terminals[arg_type])
+            if isinstance(term, EphemeralGenerator):
+                term = term()            
+            new_subtree[i] = term
     
     new_subtree[position:position+1] = individual[slice_]
     new_subtree.insert(0, new_node)
