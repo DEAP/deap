3b7e88f938b591dea04361d1e161cc28f80afa05
Félix-Antoine Fortin <felix.antoine.fortin@gmail.com>
2013-03-03 22:12:47 -0500

    Add a new version of gp.mutShrink.
    This version now works with the single list representation of
    GP PrimitiveTree.
    
    --HG--
    branch : dev

--- deap/gp.py
+++ deap/gp.py
@@ -701,10 +701,39 @@ def mutInsert(individual, pset):
     individual[slice_] = new_subtree
     return individual,
 
+def mutShrink(individual):
+    """This operator shrinks the *individual* by chosing randomly a branch and
+    replacing it with one of the branch's arguments (also randomly chosen).
+    
+    :param individual: The tree to be shrinked.
+    :returns: A tuple of one tree.
+    """    
+    # We don't want to "shrink" the root
+    if len(individual) < 3 or individual.height <= 1:
+        return individual,
+
+    iprims = []
+    for i, node in enumerate(individual[1:], 1):
+        if isinstance(node, Primitive) and node.ret in node.args:
+            iprims.append((i, node))
+
+    if len(iprims) != 0:
+        index, prim = random.choice(iprims)
+        arg_idx = random.choice([i for i, type_ in enumerate(prim.args) if type_ == prim.ret])
+        rindex = index+1
+        for _ in range(arg_idx+1):
+            rslice = individual.searchSubtree(rindex)
+            subtree = individual[rslice]
+            rindex += len(subtree)
+
+        slice_ = individual.searchSubtree(index)
+        individual[slice_] = subtree
+
+    return individual,
 
 ######################################
 # GP bloat control decorators        #
-######################################   
+######################################
 
 def staticDepthLimit(max_depth):
     """Implement a static limit on the depth of a GP tree, as defined by Koza
