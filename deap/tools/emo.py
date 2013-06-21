0dafe67cde256989dba426507b0b9233cd10137e
François-Michel De Rainville <f.derainville@gmail.com>
2013-06-21 14:45:51 -0400

    Moved selTournamentDCD to tools/emo
    --HG--
    branch : dev

--- deap/tools/emo.py
+++ deap/tools/emo.py
@@ -132,6 +132,50 @@ def assignCrowdingDist(individuals):
         individuals[i].fitness.crowding_dist = dist
 
 
+def selTournamentDCD(individuals, k):
+    """Tournament selection based on dominance (D) between two individuals, if
+    the two individuals do not interdominate the selection is made
+    based on crowding distance (CD). The *individuals* sequence length has to
+    be a multiple of 4. Starting from the beginning of the selected
+    individuals, two consecutive individuals will be different (assuming all
+    individuals in the input list are unique). Each individual from the input
+    list won't be selected more than twice.
+    
+    This selection requires the individuals to have a :attr:`crowding_dist`
+    attribute, which can be set by the :func:`assignCrowdingDist` function.
+    
+    :param individuals: A list of individuals to select from.
+    :param k: The number of individuals to select.
+    :returns: A list of selected individuals.
+    """
+    def tourn(ind1, ind2):
+        if ind1.fitness.dominates(ind2.fitness):
+            return ind1
+        elif ind2.fitness.dominates(ind1.fitness):
+            return ind2
+
+        if ind1.fitness.crowding_dist < ind2.fitness.crowding_dist:
+            return ind2
+        elif ind1.fitness.crowding_dist > ind2.fitness.crowding_dist:
+            return ind1
+
+        if random.random() <= 0.5:
+            return ind1
+        return ind2
+
+    individuals_1 = random.sample(individuals, len(individuals))
+    individuals_2 = random.sample(individuals, len(individuals))
+
+    chosen = []
+    for i in xrange(0, k, 4):
+        chosen.append(tourn(individuals_1[i],   individuals_1[i+1]))
+        chosen.append(tourn(individuals_1[i+2], individuals_1[i+3]))
+        chosen.append(tourn(individuals_2[i],   individuals_2[i+1]))
+        chosen.append(tourn(individuals_2[i+2], individuals_2[i+3]))
+
+    return chosen
+
+
 ######################################
 # Strength Pareto         (SPEA-II)  #
 ######################################
@@ -288,4 +332,4 @@ def _partition(array, begin, end):
         else:
             return j
 
-__all__ = ['selNSGA2', 'selSPEA2', 'sortNondominated']
+__all__ = ['selNSGA2', 'selTournamentDCD', 'selSPEA2', 'sortNondominated']
