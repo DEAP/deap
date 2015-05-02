f21c0937edde9ffcfbcf1275232b4c34954be856
Félix-Antoine Fortin <felix.antoine.fortin@gmail.com>
2015-05-02 08:51:48 -0400

    [doc] reinstate old theme
    See https://github.com/rtfd/readthedocs.org/issues/1202

--- doc/conf.py
+++ doc/conf.py
@@ -142,7 +142,9 @@ html_theme_path = ["_themes"]
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'classic' and 'alabaster'.
-html_theme = 'alabaster'
+html_theme = 'pydoctheme'
+#RTFD.org does not support sphinx 1.3.1 yet. 
+#html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
