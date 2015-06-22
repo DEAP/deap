a32cc7af9f1638bfb14afad68c9e610fbb329957
Luis Marti <lmarti@gmail.com>
2015-06-22 18:00:13 -0300

    weird character removed at last

--- deap/benchmarks/__init__.py
+++ deap/benchmarks/__init__.py
@@ -672,7 +672,7 @@ def dent(individual, lambda_ = 0.85):
     two attributes that take values in [-1.5, 1.5].
     From: Schuetze, O., Laumanns, M., Tantar, E., Coello Coello, C.A., & Talbi, E.-G. (2010).
     Computing gap free Pareto front approximations with stochastic search algorithms.
-    Evolutionary Computation, 18(1), 65–96. doi:10.1162/evco.2010.18.1.18103
+    Evolutionary Computation, 18(1), 65--96. doi:10.1162/evco.2010.18.1.18103
 
     Note that in that paper Dent source is stated as:
     K. Witting and M. Hessel von Molo. Private communication, 2006.
