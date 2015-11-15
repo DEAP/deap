a476f83e88ca5109667d8ac665d0f04667c64dfe
Anjum48 <anjum.sayed48@gmail.com>
2015-11-15 23:28:11 +0000

    Update gp.py

--- deap/gp.py
+++ deap/gp.py
@@ -315,7 +315,7 @@ class PrimitiveSetTyped(object):
             dict_ = self.terminals
 
         for type_ in dict_:
-            if issubclass(prim.ret, type_):
+            if issubclass(prim.ret, type_) and type_ is not bool:
                 dict_[type_].append(prim)
 
     def addPrimitive(self, primitive, in_types, ret_type, name=None):
