5bc041eb2417eb656fbfba357d8541bdb69e208d
Anjum48 <anjum.sayed48@gmail.com>
2015-11-17 19:22:38 +0000

    Additional constraint to the issubclass check

--- deap/gp.py
+++ deap/gp.py
@@ -315,7 +315,7 @@ class PrimitiveSetTyped(object):
             dict_ = self.terminals
 
         for type_ in dict_:
-            if issubclass(prim.ret, type_) and prim.ret is not bool type_ is not int:
+            if issubclass(prim.ret, type_) and prim.ret is not bool and type_ is not int:
                 dict_[type_].append(prim)
 
     def addPrimitive(self, primitive, in_types, ret_type, name=None):
