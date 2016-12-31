cfb7b5619c7d1f4fc7523b4a2cf402887dd24711
fmv1992 <fmv1992@gmail.com>
2016-12-31 01:22:34 -0200

    adapted binary to be compatible w python3

--- deap/benchmarks/binary.py
+++ deap/benchmarks/binary.py
@@ -14,6 +14,7 @@
 #    License along with DEAP. If not, see <http://www.gnu.org/licenses/>.
 
 import math
+from __future__ import division
 
 def bin2float(min_, max_, nbits):
     """Convert a binary array into an array of float where each
@@ -27,9 +28,16 @@ def bin2float(min_, max_, nbits):
     def wrap(function):
         def wrapped_function(individual, *args, **kargs):
             nelem = len(individual)/nbits
+            if nelem.is_integer():
+                nelem = int(nelem)
+            else:
+                raise ValueError(
+                    "Got an array of size %s to divide into %s parts" % (
+                        nbits, nelem))
             decoded = [0] * nelem
             for i in xrange(nelem):
-                gene = int("".join(map(str, individual[i*nbits:i*nbits+nbits])), 2)
+                gene = int("".join(map(str,
+                                       individual[i*nbits:i*nbits+nbits])), 2)
                 div = 2**nbits - 1
                 temp = float(gene)/float(div)
                 decoded[i] = min_ + (temp * (max_ - min_))
@@ -56,10 +64,10 @@ def inv_trap(individual):
 def chuang_f1(individual):
     """Binary deceptive function from : Multivariate Multi-Model Approach for
     Globally Multimodal Problems by Chung-Yao Chuang and Wen-Lian Hsu.
-    
+
     The function takes individual of 40+1 dimensions and has two global optima
     in [1,1,...,1] and [0,0,...,0].
-    """    
+    """
     total = 0
     if individual[-1] == 0:
         for i in xrange(0,len(individual)-1,4):
@@ -72,10 +80,10 @@ def chuang_f1(individual):
 def chuang_f2(individual):
     """Binary deceptive function from : Multivariate Multi-Model Approach for
     Globally Multimodal Problems by Chung-Yao Chuang and Wen-Lian Hsu.
-    
+
     The function takes individual of 40+1 dimensions and has four global optima
-    in [1,1,...,0,0], [0,0,...,1,1], [1,1,...,1] and [0,0,...,0].    
-    """    
+    in [1,1,...,0,0], [0,0,...,1,1], [1,1,...,1] and [0,0,...,0].
+    """
     total = 0
     if individual[-2] == 0 and individual[-1] == 0:
         for i in xrange(0,len(individual)-2,8):
@@ -110,7 +118,7 @@ def chuang_f3(individual):
 
 # Royal Road Functions
 def royal_road1(individual, order):
-    """Royal Road Function R1 as presented by Melanie Mitchell in : 
+    """Royal Road Function R1 as presented by Melanie Mitchell in :
     "An introduction to Genetic Algorithms".
     """
     nelem = len(individual) / order
@@ -122,7 +130,7 @@ def royal_road1(individual, order):
     return total,
 
 def royal_road2(individual, order):
-    """Royal Road Function R2 as presented by Melanie Mitchell in : 
+    """Royal Road Function R2 as presented by Melanie Mitchell in :
     "An introduction to Genetic Algorithms".
     """
     total = 0
