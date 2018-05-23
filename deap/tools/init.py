bee19008cd64946615bcd5c86c8e5c2ff0f3ddd9
François-Michel De Rainville <f.derainville@gmail.com>
2018-05-23 09:33:58 -0400

    Removed trailing spaces and added some files to ignore

--- deap/tools/init.py
+++ deap/tools/init.py
@@ -3,18 +3,18 @@ from __future__ import division
 def initRepeat(container, func, n):
     """Call the function *container* with a generator function corresponding
     to the calling *n* times the function *func*.
-    
+
     :param container: The type to put in the data from func.
     :param func: The function that will be called n times to fill the
                  container.
     :param n: The number of times to repeat func.
     :returns: An instance of the container filled with data from func.
-    
-    This helper function can can be used in conjunction with a Toolbox 
-    to register a generator of filled containers, as individuals or 
+
+    This helper function can can be used in conjunction with a Toolbox
+    to register a generator of filled containers, as individuals or
     population.
-    
-        >>> initRepeat(list, random.random, 2) # doctest: +ELLIPSIS, 
+
+        >>> initRepeat(list, random.random, 2) # doctest: +ELLIPSIS,
         ...                                    # doctest: +NORMALIZE_WHITESPACE
         [0.4761..., 0.6302...]
 
@@ -23,18 +23,18 @@ def initRepeat(container, func, n):
     return container(func() for _ in xrange(n))
 
 def initIterate(container, generator):
-    """Call the function *container* with an iterable as 
-    its only argument. The iterable must be returned by 
+    """Call the function *container* with an iterable as
+    its only argument. The iterable must be returned by
     the method or the object *generator*.
-    
+
     :param container: The type to put in the data from func.
     :param generator: A function returning an iterable (list, tuple, ...),
                       the content of this iterable will fill the container.
     :returns: An instance of the container filled with data from the
               generator.
-    
-    This helper function can can be used in conjunction with a Toolbox 
-    to register a generator of filled containers, as individuals or 
+
+    This helper function can can be used in conjunction with a Toolbox
+    to register a generator of filled containers, as individuals or
     population.
 
         >>> from random import sample
@@ -51,18 +51,18 @@ def initIterate(container, generator):
 def initCycle(container, seq_func, n=1):
     """Call the function *container* with a generator function corresponding
     to the calling *n* times the functions present in *seq_func*.
-    
+
     :param container: The type to put in the data from func.
     :param seq_func: A list of function objects to be called in order to
                      fill the container.
     :param n: Number of times to iterate through the list of functions.
     :returns: An instance of the container filled with data from the
               returned by the functions.
-    
-    This helper function can can be used in conjunction with a Toolbox 
-    to register a generator of filled containers, as individuals or 
+
+    This helper function can can be used in conjunction with a Toolbox
+    to register a generator of filled containers, as individuals or
     population.
-    
+
         >>> func_seq = [lambda:1 , lambda:'a', lambda:3]
         >>> initCycle(list, func_seq, n=2)
         [1, 'a', 3, 1, 'a', 3]
@@ -79,8 +79,8 @@ if __name__ == "__main__":
     import random
     random.seed(64)
     doctest.run_docstring_examples(initRepeat, globals())
-    
+
     random.seed(64)
     doctest.run_docstring_examples(initIterate, globals())
     doctest.run_docstring_examples(initCycle, globals())
-    
+
--- deap/tools/selection.py
+++ deap/tools/selection.py
@@ -13,11 +13,11 @@ def selRandom(individuals, k):
     """Select *k* individuals at random from the input *individuals* with
     replacement. The list returned contains references to the input
     *individuals*.
-    
+
     :param individuals: A list of individuals to select from.
     :param k: The number of individuals to select.
     :returns: A list of selected individuals.
-    
+
     This function uses the :func:`~random.choice` function from the
     python base :mod:`random` module.
     """
@@ -39,7 +39,7 @@ def selBest(individuals, k, fit_attr="fitness"):
 def selWorst(individuals, k, fit_attr="fitness"):
     """Select the *k* worst individuals among the input *individuals*. The
     list returned contains references to the input *individuals*.
-    
+
     :param individuals: A list of individuals to select from.
     :param k: The number of individuals to select.
     :param fit_attr: The attribute of individuals to use as selection criterion
@@ -52,13 +52,13 @@ def selTournament(individuals, k, tournsize, fit_attr="fitness"):
     """Select the best individual among *tournsize* randomly chosen
     individuals, *k* times. The list returned contains
     references to the input *individuals*.
-    
+
     :param individuals: A list of individuals to select from.
     :param k: The number of individuals to select.
     :param tournsize: The number of individuals participating in each tournament.
     :param fit_attr: The attribute of individuals to use as selection criterion
     :returns: A list of selected individuals.
-    
+
     This function uses the :func:`~random.choice` function from the python base
     :mod:`random` module.
     """
@@ -73,17 +73,17 @@ def selRoulette(individuals, k, fit_attr="fitness"):
     spins of a roulette. The selection is made by looking only at the first
     objective of each individual. The list returned contains references to
     the input *individuals*.
-    
+
     :param individuals: A list of individuals to select from.
     :param k: The number of individuals to select.
     :param fit_attr: The attribute of individuals to use as selection criterion
     :returns: A list of selected individuals.
-    
+
     This function uses the :func:`~random.random` function from the python base
     :mod:`random` module.
-    
+
     .. warning::
-       The roulette selection by definition cannot be used for minimization 
+       The roulette selection by definition cannot be used for minimization
        or when the fitness can be smaller or equal to 0.
     """
 
@@ -98,7 +98,7 @@ def selRoulette(individuals, k, fit_attr="fitness"):
             if sum_ > u:
                 chosen.append(ind)
                 break
-    
+
     return chosen
 
 
@@ -107,10 +107,10 @@ def selDoubleTournament(individuals, k, fitness_size, parsimony_size, fitness_fi
     to discriminate good solutions. This kind of tournament is obviously
     useless with fixed-length representation, but has been shown to
     significantly reduce excessive growth of individuals, especially in GP,
-    where it can be used as a bloat control technique (see 
-    [Luke2002fighting]_). This selection operator implements the double 
+    where it can be used as a bloat control technique (see
+    [Luke2002fighting]_). This selection operator implements the double
     tournament technique presented in this paper.
-    
+
     The core principle is to use a normal tournament selection, but using a
     special sample function to select aspirants, which is another tournament
     based on the size of the individuals. To ensure that the selection
@@ -119,11 +119,11 @@ def selDoubleTournament(individuals, k, fitness_size, parsimony_size, fitness_fi
     case, the smaller individual among two will be selected with a probability
     *size_tourn_size*/2. For instance, if *size_tourn_size* is set to 1.4,
     then the smaller individual will have a 0.7 probability to be selected.
-    
+
     .. note::
         In GP, it has been shown that this operator produces better results
         when it is combined with some kind of a depth limit.
-    
+
     :param individuals: A list of individuals to select from.
     :param k: The number of individuals to select.
     :param fitness_size: The number of individuals participating in each \
@@ -139,8 +139,8 @@ def selDoubleTournament(individuals, k, fitness_size, parsimony_size, fitness_fi
     (see [Luke2002fighting]_).
     :param fit_attr: The attribute of individuals to use as selection criterion
     :returns: A list of selected individuals.
-    
-    .. [Luke2002fighting] Luke and Panait, 2002, Fighting bloat with 
+
+    .. [Luke2002fighting] Luke and Panait, 2002, Fighting bloat with
         nonparametric parsimony pressure
     """
     assert (1 <= parsimony_size <= 2), "Parsimony tournament size has to be in the range [1, 2]."
@@ -164,14 +164,14 @@ def selDoubleTournament(individuals, k, fitness_size, parsimony_size, fitness_fi
             chosen.append(ind1 if random.random() < prob else ind2)
 
         return chosen
-    
+
     def _fitTournament(individuals, k, select):
         chosen = []
         for i in xrange(k):
             aspirants = select(individuals, k=fitness_size)
             chosen.append(max(aspirants, key=attrgetter(fit_attr)))
         return chosen
-    
+
     if fitness_first:
         tfit = partial(_fitTournament, select=selRandom)
         return _sizeTournament(individuals, k, tfit)
@@ -212,7 +212,7 @@ def selStochasticUniversalSampling(individuals, k, fit_attr="fitness"):
     return chosen
 
 def selLexicase(individuals, k):
-    """Returns an individual that does the best on the fitness cases when 
+    """Returns an individual that does the best on the fitness cases when
     considered one at a time in random order.
     http://faculty.hampshire.edu/lspector/pubs/lexicase-IEEE-TEC.pdf
 
@@ -220,33 +220,33 @@ def selLexicase(individuals, k):
     :param k: The number of individuals to select.
     :returns: A list of selected individuals.
     """
-    selected_individuals = []    
-    
+    selected_individuals = []
+
     for i in range(k):
         fit_weights = individuals[0].fitness.weights
-        
+
         candidates = individuals
         cases = list(range(len(individuals[0].fitness.values)))
         random.shuffle(cases)
-        
+
         while len(cases) > 0 and len(candidates) > 1:
-            f = min        
+            f = min
             if fit_weights[cases[0]] > 0:
                 f = max
-            
-            best_val_for_case = f(map(lambda x: x.fitness.values[cases[0]], candidates)) 
-            
+
+            best_val_for_case = f(map(lambda x: x.fitness.values[cases[0]], candidates))
+
             candidates = list(filter(lambda x: x.fitness.values[cases[0]] == best_val_for_case, candidates))
             cases.pop(0)
-                     
+
         selected_individuals.append(random.choice(candidates))
-    
+
     return selected_individuals
 
 
 def selEpsilonLexicase(individuals, k, epsilon):
     """
-    Returns an individual that does the best on the fitness cases when 
+    Returns an individual that does the best on the fitness cases when
     considered one at a time in random order. Requires a epsilon parameter.
     https://push-language.hampshire.edu/uploads/default/original/1X/35c30e47ef6323a0a949402914453f277fb1b5b0.pdf
     Implemented epsilon_y implementation.
@@ -254,69 +254,69 @@ def selEpsilonLexicase(individuals, k, epsilon):
     :param individuals: A list of individuals to select from.
     :param k: The number of individuals to select.
     :returns: A list of selected individuals.
-    """      
-    selected_individuals = []    
-    
+    """
+    selected_individuals = []
+
     for i in range(k):
         fit_weights = individuals[0].fitness.weights
-        
+
         candidates = individuals
         cases = list(range(len(individuals[0].fitness.values)))
         random.shuffle(cases)
-        
+
         while len(cases) > 0 and len(candidates) > 1:
             if fit_weights[cases[0]] > 0:
-                best_val_for_case = max(map(lambda x: x.fitness.values[cases[0]], candidates)) 
+                best_val_for_case = max(map(lambda x: x.fitness.values[cases[0]], candidates))
                 min_val_to_survive_case = best_val_for_case - epsilon
                 candidates = list(filter(lambda x: x.fitness.values[cases[0]] >= min_val_to_survive_case, candidates))
             else :
-                best_val_for_case = min(map(lambda x: x.fitness.values[cases[0]], candidates)) 
+                best_val_for_case = min(map(lambda x: x.fitness.values[cases[0]], candidates))
                 max_val_to_survive_case = best_val_for_case + epsilon
                 candidates = list(filter(lambda x: x.fitness.values[cases[0]] <= max_val_to_survive_case, candidates))
-            
+
             cases.pop(0)
-                     
+
         selected_individuals.append(random.choice(candidates))
-    
+
     return selected_individuals
 
 def selAutomaticEpsilonLexicase(individuals, k):
     """
     Returns an individual that does the best on the fitness cases when considered one at a
-    time in random order. 
+    time in random order.
     https://push-language.hampshire.edu/uploads/default/original/1X/35c30e47ef6323a0a949402914453f277fb1b5b0.pdf
     Implemented lambda_epsilon_y implementation.
 
     :param individuals: A list of individuals to select from.
     :param k: The number of individuals to select.
     :returns: A list of selected individuals.
-    """      
-    selected_individuals = []    
-    
+    """
+    selected_individuals = []
+
     for i in range(k):
         fit_weights = individuals[0].fitness.weights
-        
+
         candidates = individuals
         cases = list(range(len(individuals[0].fitness.values)))
         random.shuffle(cases)
 
-        while len(cases) > 0 and len(candidates) > 1: 
+        while len(cases) > 0 and len(candidates) > 1:
             errors_for_this_case = [x.fitness.values[cases[0]] for x in candidates]
             median_val = np.median(errors_for_this_case)
             median_absolute_deviation = np.median([abs(x - median_val) for x in errors_for_this_case])
             if fit_weights[cases[0]] > 0:
-                best_val_for_case = max(errors_for_this_case) 
+                best_val_for_case = max(errors_for_this_case)
                 min_val_to_survive = best_val_for_case - median_absolute_deviation
                 candidates = list(filter(lambda x: x.fitness.values[cases[0]] >= min_val_to_survive, candidates))
             else :
-                best_val_for_case = min(errors_for_this_case) 
+                best_val_for_case = min(errors_for_this_case)
                 max_val_to_survive = best_val_for_case + median_absolute_deviation
                 candidates = list(filter(lambda x: x.fitness.values[cases[0]] <= max_val_to_survive, candidates))
-            
+
             cases.pop(0)
-                     
+
         selected_individuals.append(random.choice(candidates))
-    
+
     return selected_individuals
 
 
