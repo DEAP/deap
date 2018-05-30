42162f404323ff2bbf3d65a62c4ccdb4633736b7
François-Michel De Rainville <f.derainville@gmail.com>
2018-05-30 16:36:38 -0400

    Show warning only if hypervolume is called

--- deap/tools/_hypervolume/pyhv.py
+++ deap/tools/_hypervolume/pyhv.py
@@ -28,14 +28,12 @@ import warnings
 
 import numpy
 
-warnings.simplefilter("once", ImportWarning)
-warnings.warn("Falling back to the python version of hypervolume "
-    "module. Expect this to be very slow.", ImportWarning)
-
 def hypervolume(pointset, ref):
     """Compute the absolute hypervolume of a *pointset* according to the
     reference point *ref*.
     """
+    warnings.warn("Falling back to the python version of hypervolume "
+        "module. Expect this to be very slow.", RuntimeWarning)
     hv = _HyperVolume(ref)
     return hv.compute(pointset)
 
