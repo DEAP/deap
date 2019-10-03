79692da9dc9924161af7e101549a1bc7bc8dcc45
loganthomas <logan.thomas005@gmail.com>
2019-10-02 20:31:50 -0500

    fix typos in cxUniform docstring

--- deap/tools/crossover.py
+++ deap/tools/crossover.py
@@ -72,12 +72,12 @@ def cxTwoPoints(ind1, ind2):
 
 def cxUniform(ind1, ind2, indpb):
     """Executes a uniform crossover that modify in place the two
-    :term:`sequence` individuals. The attributes are swapped accordingto the
+    :term:`sequence` individuals. The attributes are swapped according to the
     *indpb* probability.
 
     :param ind1: The first individual participating in the crossover.
     :param ind2: The second individual participating in the crossover.
-    :param indpb: Independent probabily for each attribute to be exchanged.
+    :param indpb: Independent probability for each attribute to be exchanged.
     :returns: A tuple of two individuals.
 
     This function uses the :func:`~random.random` function from the python base
