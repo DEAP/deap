52bc2e24273a5e44c928763666a2dea1d5429219
Andriy <kamua@users.noreply.github.com>
2020-01-31 16:08:11 +0200

    Fix errors in the cxOrdered function
    Proposed error fixing is according to [Goldberg1989]. It were tested on the example in page 174 of [Goldberg1989].

--- deap/tools/crossover.py
+++ deap/tools/crossover.py
@@ -213,21 +213,21 @@ def cxOrdered(ind1, ind2):
     if a > b:
         a, b = b, a
 
-    holes1, holes2 = [True] * size, [True] * size
+    holes1, holes2 = [False] * size, [False] * size
     for i in range(size):
         if i < a or i > b:
-            holes1[ind2[i]] = False
-            holes2[ind1[i]] = False
+            holes1[i] = ind1[i] in ind2[a : b + 1]
+            holes2[i] = ind2[i] in ind1[a : b + 1]
 
     # We must keep the original values somewhere before scrambling everything
     temp1, temp2 = ind1, ind2
     k1, k2 = b + 1, b + 1
     for i in range(size):
-        if not holes1[temp1[(i + b + 1) % size]]:
+        if not holes1[(i + b + 1) % size]:
             ind1[k1 % size] = temp1[(i + b + 1) % size]
             k1 += 1
 
-        if not holes2[temp2[(i + b + 1) % size]]:
+        if not holes2[(i + b + 1) % size]:
             ind2[k2 % size] = temp2[(i + b + 1) % size]
             k2 += 1
 
