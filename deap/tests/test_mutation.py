750b103fa5877e54c87c19ea6af3e168d3490d3c
James Alexander Hughes <jameshughes89@Gmail.com>
2022-04-19 11:11:33 -0300

    mutInversion tests

---
+++ deap/tests/test_mutation.py
@@ -0,0 +1,43 @@
+import unittest
+import mock
+
+from deap.tools.mutation import mutInversion
+
+class MutationTest(unittest.TestCase):
+
+    def test_mutInverstion_empty_chromosome_raises_value_error_from_randrange(self):
+        with self.assertRaises(ValueError, msg="empty range for randrange()"):
+            mutInversion([])
+
+    def test_mutInversion_size_one_chromosome_returns_unchanged_chromosome_in_tuple(self):
+        chromosome = ["a"]
+        expected = ["a"]
+        self.assertEqual((expected,), mutInversion(chromosome))
+
+    @mock.patch("random.randrange")
+    def test_mutInversion_same_random_indices_returns_unchanged_chromosome_in_tuple(self, mock_randrange):
+        mock_randrange.side_effect = [2, 2]
+        chromosome = ["a", "b", "c", "d", "e"]
+        expected = ["a", "b", "c", "d", "e"]
+        self.assertEqual((expected,), mutInversion(chromosome))
+
+    @mock.patch("random.randrange")
+    def test_mutInversion_difference_of_one_random_indices_returns_unchanged_chromosome_in_tuple(self, mock_randrange):
+        mock_randrange.side_effect = [2, 3]
+        chromosome = ["a", "b", "c", "d", "e"]
+        expected = ["a", "b", "c", "d", "e"]
+        self.assertEqual((expected,), mutInversion(chromosome))
+
+    @mock.patch("random.randrange")
+    def test_mutInversion_full_length_random_indices_returns_reversed_chromosome_in_tuple(self, mock_randrange):
+        mock_randrange.side_effect = [0, 5]
+        chromosome = ["a", "b", "c", "d", "e"]
+        expected = ["e", "d", "c", "b", "a"]
+        self.assertEqual((expected,), mutInversion(chromosome))
+
+    @mock.patch("random.randrange")
+    def test_mutInversion_general_case_returns_correctly_mutated_chromosome_in_tuple(self, mock_randrange):
+        mock_randrange.side_effect = [1, 4]
+        chromosome = ["a", "b", "c", "d", "e"]
+        expected = ["a", "d", "c", "b", "e"]
+        self.assertEqual((expected,), mutInversion(chromosome))
