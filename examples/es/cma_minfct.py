b2e96f26c309e6018502c73ac1fc2c270f6d37a9
Ben Elliston <bje@air.net.au>
2022-08-02 23:36:43 +1000

    Revert "cma_minfct.py: Remove old call to tools.EvolutionLogger. Fixes #504."
    This reverts commit bc5737ce326cc4aa8795f903db74af2c5af5d433.

--- examples/es/cma_minfct.py
+++ examples/es/cma_minfct.py
@@ -49,6 +49,8 @@ def main():
     stats.register("min", numpy.min)
     stats.register("max", numpy.max)
 
+    #logger = tools.EvolutionLogger(stats.functions.keys())
+   
     # The CMA-ES algorithm converge with good probability with those settings
     algorithms.eaGenerateUpdate(toolbox, ngen=250, stats=stats, halloffame=hof)
 
